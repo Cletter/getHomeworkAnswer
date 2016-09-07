71c6b82843ac8958cb7d316d4ad78a11e09d937d
Cletter <269609846@qq.com>
2016-09-07 16:38:59 +0800

    Create getHomeworkAnswer.php

---
+++ php/getHomeworkAnswer.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>与学霸对答案</title>
+<meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-status-bar-style" content="black">
+<meta http-equiv="Cache-Control" content="no-siteapp">
+<link rel="stylesheet" href="http://www.lolhelper.cn/dist/css/ratchet3.min.css">
+<link rel="stylesheet" href="http://www.lolhelper.cn/dist/css/style.css">
+</head>
+ <body>
+<header class="bar bar-nav">
+<h1 class="title"><strong>与学霸对答案</strong></h1>
+</header>
+
+<div class="card" style="border:none">
+
+<iframe src=<?php echo "http://elearning.njmu.edu.cn/g2s/CourseLive/Test/HomeWorkAdd?TestID=".$_GET['id']."&OCID="; ?> width="100%" height="99999"  frameborder="0" scrolling="yes" style="position: absolute; top: -250px; left: -0px;"></iframe>
+
+</body>
+</html>
+
